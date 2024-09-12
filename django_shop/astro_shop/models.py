0ae982b55249c92b14548e362b4b658a99ad24dc
Ivan <itverdyy@gmail.com>
2024-09-12 16:33:17 +0300

    created category class, thinking about customer, product, order classes

--- django_shop/astro_shop/models.py
+++ django_shop/astro_shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+#categories
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+#customers
+class Customer(models.Model):
+
+#products
+class Product(models.Model):
+
+#orders
+class Order(models.Model):
