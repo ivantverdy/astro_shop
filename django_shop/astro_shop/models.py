2654f01c5c4a85f6e334003bb10bb1ef7321b396
Ivan <itverdyy@gmail.com>
2024-09-12 16:47:25 +0300

    filled customer class

--- django_shop/astro_shop/models.py
+++ django_shop/astro_shop/models.py
@@ -8,6 +8,14 @@ class Category(models.Model):
         return self.name
 #customers
 class Customer(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField(max_length=128)
+    phone = models.CharField(max_length=16)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 #products
 class Product(models.Model):
