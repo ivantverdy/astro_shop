6b9ec872852e7bbbea43707cce25ddf3296abb87
Ivan <itverdyy@gmail.com>
2024-09-12 17:25:05 +0300

    decided to go without registration of a customer, just unauthorized users => added info in Order class

--- django_shop/astro_shop/models.py
+++ django_shop/astro_shop/models.py
@@ -1,23 +1,16 @@
 from django.db import models
 import datetime
 
-#categories
+
+# categories
 class Category(models.Model):
     name = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
-#customers
-class Customer(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    email = models.EmailField(max_length=128)
-    phone = models.CharField(max_length=16)
-    password = models.CharField(max_length=128)
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
 
-#products
+# products
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=256, default='', blank=True, null=True)
@@ -28,5 +21,17 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-#orders
+
+# orders
 class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField(max_length=128)
+    phone = models.CharField(max_length=16)
+    address = models.CharField(max_length=256)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} ordered {self.product.name}'
