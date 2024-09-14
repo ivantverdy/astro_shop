875bec354450f2581026bc486b2e5524754ce556
Ivan <itverdyy@gmail.com>
2024-09-14 14:52:14 +0300

    created login_user and logout_userin views and urls

--- django_shop/astro_shop/views.py
+++ django_shop/astro_shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -9,3 +11,11 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    pass
